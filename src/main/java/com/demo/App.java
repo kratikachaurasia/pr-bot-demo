e5863dbd6f0cb1cd5c306c91f9987c0c5f358bfc
Kratika <kratikachaurasia25@gmail.com>
2025-09-22 17:22:33 +0530

    gwork-1demo

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,7 +2,8 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        
+        int x = 10;
+        int y=11;
         System.out.println("Hello from PR Bot Demo!");
     }
 }
